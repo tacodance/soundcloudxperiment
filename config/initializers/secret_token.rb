6faf6104930f7ae0b7aaf7fac897280d8c45a54b
Antonio <tacodance@gmail.com>
2012-01-06 11:59:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundcloudxperiment::Application.config.secret_token = '99e6a76c55b8007f4e7bb75b2ec0246e51a256233fc55c2f116f09dd9acbb8794199da213c928b0ac8550e33d9490063565c42d010ee7ce648c23c44517c0e8d'
